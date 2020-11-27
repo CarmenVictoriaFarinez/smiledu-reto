622bf7d8d300a5e28f247e9b037b3946ddc99cb7
CarmenVictoriaFarinez <acuario1140@gmail.com>
2020-11-27 01:06:13 -0500

    config sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+// Angular material
+import {MatSidenavModule} from '@angular/material/sidenav';
+// Components
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
